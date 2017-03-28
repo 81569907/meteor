4c8cccabbc55730cc1c7c4da8c96f840ce65488e
yuanyuntian <yuanf1990@163.com>
2017-03-28 18:09:18 +0800

    1.添加官方示例

---
+++ Mongo/server/main.js
@@ -0,0 +1,18 @@
+import { Meteor } from 'meteor/meteor';
+
+Players = new Mongo.Collection("players");
+
+Meteor.startup(() => {
+  // code to run on server at startup
+    if(Players.find().count() === 0){
+        var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
+            "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+
+        _.each(names, function (name) {
+            Players.insert({
+                name:name,
+                score:Math.floor(Random.fraction() * 10)*5
+            });
+        });
+    }
+});
